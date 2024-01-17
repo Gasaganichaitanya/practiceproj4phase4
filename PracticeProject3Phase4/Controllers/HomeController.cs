f720b04be80ff83bd5cacf5a9763bd01202405de
Gsaganichaitanya <chaitanyagasagani@gmail.com>
2024-01-17 17:18:54 +0530

    practiceproj4phase4

---
+++ PracticeProject3Phase4/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PracticeProject3Phase4.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Welcome to Joe's Pizza";
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Pizza is a very delicious Food.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
